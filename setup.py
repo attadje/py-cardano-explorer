9a91b96b5575a5aafff1018559438138982ce564
ATTA <djessyatta@live.fr>
2021-10-02 13:12:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import find_packages, setuptools
 
-with open('PYPI_README.md') as f:
-    long_description = f.read()
+try:
+    with open('PYPI_README.md') as f:
+        long_description = f.read()
+except Exception as e:
+    long_description=""
 
 setuptools.setup(
     name='cardano_explorer',
     packages=find_packages(include=['cardano_explorer']),
-    version='0.1.11',
+    version='0.1.12',
     description='Python wrapper for accessing and processing information stored on the Cardano blockchain using Blockfrost API.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Djessy ATTA',
     author_email = 'djessyatta@live.fr',
     url = 'https://github.com/djessy-atta/py-cardano-explorer',
-    download_url = 'https://github.com/djessy-atta/py-cardano-explorer/archive/refs/tags/v0.1-beta.11.tar.gz',
+    download_url = 'https://github.com/djessy-atta/py-cardano-explorer/archive/refs/tags/v0.1-beta.12.tar.gz',
     keywords = ['CARDANO', 'API', 'WRAPPER', 'BLOCKCHAIN', 'BLOCKFROST'],
     license='MIT',
     install_requires=['pandas>=1.3.2', 'requests>=2.26.0', 'typing>=3.7.4.3', 'numpy==1.21.2', 'tqdm>=4.62.2'],
