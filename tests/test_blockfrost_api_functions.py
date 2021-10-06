c0529046df5d33c5c098d353bdeaa73cf8878277
ATTA <djessyatta@live.fr>
2021-10-06 21:47:16 +0200

    Create ne test for the blockfrost api functions

--- tests/test_blockfrost_api_functions.py
+++ tests/test_blockfrost_api_functions.py
@@ -6,12 +6,19 @@ from cardano_explorer import blockfrost_api
 # Check if the Blockfrost API Key is configured in a environmental variable 
 assert (os.getenv('BLOCKFROST_API_KEY') is not None), '[ERROR] Your blockfrost api key is not configured in your environement path.'
 
-# Random pool_id, address, stake_address find in the cardano network
 pool_id= "pool1ekhy5xsgjaq38em75vevk8df0k0rljju77tljw288ys5kumqce5"
 address= 'addr1q8z24xgrlj3m2qjh2vxyqg2fh33y3tegufkll5c4lu8u35gkhpw3h4yhn93ve2whllg0wjazjs5jj8332mgqe332f3uq8m7m6h'
 stake_address= 'stake1u8uzevd539lxn40jt60g72a649zdphe9e8hrye4nf5jv0js9uzhzg'
 policy_id='40fa2aa67258b4ce7b5782f74831d46a84c59a0ff0c28262fab21728'
 asset_name='436c61794e6174696f6e33393836'
+tx_hash='117f97ccf6e98a16697e7cc205daf2d0bfe83d849a63df2f40d10bef235848e7'
+stake_tx_hash='97a774aa60a2926c9949bfe1edf1dcc2f2297d36633e14a87ae449b4158a027f'
+script_tx_hash_redeemers = '34dd0b7ae56b65f4cda029d99cda8322684ec6339ac1c195c16458c1e5e94b96'
+stake_mir_tx_hash = 'c041e475d161444a6a8ca9005ef3deb36ebd579c347d90b5b53968d47d5193da'
+stake_withdrawal_tx_hash = '80b09b61d2da86f5847d0b9a5f72d32224fcd7e1aa17161b9d24732339a26836'
+script_redeemer_tx_hash = '34dd0b7ae56b65f4cda029d99cda8322684ec6339ac1c195c16458c1e5e94b96'
+script_tx_hash = 'cc7888851f0f5aa64c136e0c8fb251e9702f3f6c9efcf3a60a54f419'
+stake_pool_registration_tx_hash = 'b1bfffc26b6210ced9cc679781922e8b1ac70a2f7719523528639da4ab7f2d88'
 
 cardano_mainnet = blockfrost_api.Auth()
 
@@ -107,7 +114,33 @@ class Test(unittest.TestCase):
          print("***********************************************************")
          print("[INFO] Test of the function 'assets_policy_informations'")
          print("***********************************************************")
-         self.assertTrue(isinstance(cardano_mainnet.assets_policy_informations('762fd23f1de309673a847a0c16e45647549effa84befa7dec4f7da8e', pandas=True), tuple))
+         self.assertTrue(isinstance(cardano_mainnet.assets_policy_info(policy_id, nb_of_results=100), list))
+
+    def test_transaction(self):
+         print("********************************")
+         print("[INFO] Test transaction functions")
+         print("********************************")
+         self.assertTrue(isinstance(cardano_mainnet.specific_tx(tx_hash), dict))
+         self.assertTrue(isinstance(cardano_mainnet.tx_utxos(tx_hash), dict))
+         self.assertTrue(isinstance(cardano_mainnet.tx_stake_address_cert(stake_tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_delegation_cert(stake_tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_withdrawal_url(stake_withdrawal_tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_transaction_mirs(stake_mir_tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_stake_pool_update(script_tx_hash_redeemers), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_stake_pool_retirement_cert(stake_tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_metadata(tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_stake_pool_update(script_tx_hash_redeemers), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_metadata(tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_cbor_metadata(tx_hash), list))
+         self.assertTrue(isinstance(cardano_mainnet.tx_redeemers(script_redeemer_tx_hash), list))
+
+    def test_script(self):
+        print("****************************")
+        print("[INFO] Test script functions")
+        print("****************************")
+        self.assertTrue(isinstance(cardano_mainnet.scripts_list(), dict))
+        self.assertTrue(isinstance(cardano_mainnet.specific_script(script_tx_hash), dict))
+        self.assertTrue(isinstance(cardano_mainnet.redeem_specific_script(script_tx_hash), dict))
 
 
 if __name__ == '__main__':
